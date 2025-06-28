1bcbbc13f088db890c1f1fa7502ba6284bd38d95
root <root@jekyll.toger.us>
2025-06-28 02:57:32 +0000

    Fix link

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751078609',resources: [
+    cacheName: 'chirpy-1751079440',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

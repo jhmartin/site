4414dbbd93e72ccf0703bf9c9fba778c4600955a
root <root@jekyll.toger.us>
2025-06-28 02:43:49 +0000

    Fix site url

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751075488',resources: [
+    cacheName: 'chirpy-1751078609',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

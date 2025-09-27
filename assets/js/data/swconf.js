7799e259eb5f0f073db71f0b0adc4fb49abd3457
root <root@jekyll.toger.us>
2025-09-27 21:09:29 +0000

    Update franklinwh, add 'just' post.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758988168',resources: [
+    cacheName: 'chirpy-1759007342',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
